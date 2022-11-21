4f270400015508ca049443286c8a617c751222e4
kemaltt <ttnckemal@gmail.com>
2022-11-21 19:04:04 +0100

    update

--- src/components/WorkSpace.jsx
+++ src/components/WorkSpace.jsx
@@ -1,33 +1,30 @@
 import React from "react";
+import { CgAttachment } from "react-icons/cg";
 
-export default function WorkSpace({ workSpace,setToggle }) {
-
-    const createWorkSpace=()=>{
-        setToggle(false)
-    }
+export default function WorkSpace({ workSpace, setToggle }) {
+  const createWorkSpace = () => {
+    setToggle(false);
+  };
 
   return (
     <div className="work_space">
       {workSpace.map((el, i) => (
- 
         <div
           key={i}
           style={{ background: i % 2 === 1 ? "#c16ecf" : "#0e76ec" }}
           className="work_space_card"
         >
+          <CgAttachment style={{ fontSize: "1.5rem" }} />
           <h3>{el.workSpaceName}</h3>
         </div>
-
-    
-     
       ))}
-          <div
+      <div
         onClick={createWorkSpace}
-          style={{ background:  "#5C17CF" }}
-          className="work_space_card"
-        >
-          <h3 >Create</h3>
-        </div>
+        style={{ background: "#5C17CF" }}
+        className="work_space_card"
+      >
+        <h3>Create</h3>
+      </div>
     </div>
   );
 }

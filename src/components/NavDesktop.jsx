b9991725099ed4fc3f628c1d612b2056c9dc9aba
kemaltt <ttnckemal@gmail.com>
2022-12-04 01:04:30 +0100

    commit

--- src/components/NavDesktop.jsx
+++ src/components/NavDesktop.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 // import { MdLogout } from 'react-icons/md'
 
 export default function NavDesktop({ setIsLogin, isLogin, setLoading }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   // const handleLogOut = () => {
   //   // localStorage.clear()
   //   setTimeout(() => {
@@ -15,14 +15,14 @@ export default function NavDesktop({ setIsLogin, isLogin, setLoading }) {
   // }
   return (
     <div className="nav_desktop">
-      <div onClick={() => navigate('/')} className="logo">
+      <div onClick={() => navigate("/")} className="logo">
         {/* <img src="trello.png" alt="logo" /> */}
         <h4>IT4US</h4>
         <h2>TODO</h2>
       </div>
       <ul>
-        <Link to={'/about'}>About</Link>
-        <Link to={'/contact'}>Contact</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
       </ul>
 
       {/* <div className="nav_items_auth">
@@ -38,5 +38,5 @@ export default function NavDesktop({ setIsLogin, isLogin, setLoading }) {
         )}
       </div> */}
     </div>
-  )
+  );
 }

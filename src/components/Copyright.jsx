f62ff392741726074422fdd803c6714ccee768dd
kemaltt <ttnckemal@gmail.com>
2022-12-11 22:02:54 +0100

    footer styling update

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import Typography from '@mui/material/Typography'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
 
 export default function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" {...props}>
-      {'Copyright © '}
+      {"Copyright © "}
 
-      <Link style={{ textDecoration: 'none', color: 'white' }} to={'/privacy'}>
-        {' '}
+      <Link
+        style={{ textDecoration: "none", color: "white", marginRight: "4px" }}
+        to={"/privacy"}
+      >
+        {" "}
         Privacy Policy
       </Link>
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
-  )
+  );
 }
